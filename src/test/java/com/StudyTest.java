0aaf4b04cf65f309b02d92f3b5c086273365e161
geombong <shoy1415@gmail.com>
2022-03-05 20:21:35 +0900

    자바 테스트 스터디 시작

---
+++ src/test/java/com/StudyTest.java
@@ -0,0 +1,22 @@
+package com;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class StudyTest {
+
+    @Test
+    void create() {
+        Study study = new Study(10);
+        assertAll(
+                () -> assertNotNull(study),
+                () -> assertEquals(StudyStatus.DRAFT, study.getStatus(),
+                        () -> "스터디를 처음만들면 " + StudyStatus.DRAFT + " 상태여야 한다."),
+                () -> assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야 한다.")
+        );
+    }
+}
